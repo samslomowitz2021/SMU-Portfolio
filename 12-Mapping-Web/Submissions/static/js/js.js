e2f799b4fea187033f640fca815a914198ea2a20
unknown <samslomowitz2021@icloud.com>
2022-04-12 10:21:10 -0500

    almost done with leaflet hw

--- 12-Mapping-Web/Submissions/static/js/js.js
+++ 12-Mapping-Web/Submissions/static/js/js.js
@@ -75,7 +75,7 @@ function Map(info) {
     var circles = [];
     for (let i = 0; i < events.length; i++) {
         let event = events[i];
-        let circle_color = "#4E878C";
+        let circle_color = getColor(event.properties.mag);
 
         let location = [event.geometry.coordinates[1], event.geometry.coordinates[0]]
 
@@ -83,10 +83,12 @@ function Map(info) {
             color: circle_color,
             fillColor: circle_color,
             fillOpacity: 0.75,
-            radius: 50000
+            radius: getRadius(event.properties.mag)
+            
         }).bindPopup(`<h3><strong>${ event.properties.title }</strong></h3><hr><p><em>${new Date(event.properties.time)}</em></p >`);
         circles.push(circle);
     }
+        
 
     var circleLayer = L.layerGroup(circles);
 
@@ -117,5 +119,51 @@ function Map(info) {
 
     // Create a layer control that contains our baseMaps.
     // Be sure to add an overlay Layer that contains the earthquake GeoJSON.
+    var legend = L.control({ position: "bottomleft" });
+
     L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+
+    legend.onAdd = function(map) {
+        var div = L.DomUtil.create("div", "legend");
+        div.innerHTML += "<h4>Richter Scale</h4>";
+        div.innerHTML += '<i style="background: #477AC2"></i><span>-10 to 10</span><br>';
+        div.innerHTML += '<i style="background: #448D40"></i><span>10 to 30</span><br>';
+        div.innerHTML += '<i style="background: #E6E696"></i><span>30 to 50</span><br>';
+        div.innerHTML += '<i style="background: #E8E6E0"></i><span>50 to 70</span><br>';
+        div.innerHTML += '<i style="background: #FFFFFF"></i><span>70 to 90</span><br>';
+        div.innerHTML += '<i style="background: #FFFFFF"></i><span>90+</span><br>';
+        
+        
+        
+      
+        return div;
+      };
+      
+      legend.addTo(myMap);
+    }
+
+function getRadius(mag) {
+        return mag * 10000
 }
+
+function getColor(mag) {
+    let color2 = '#058C42';
+
+    if (mag <= 1.0) {
+        color2 = "red";
+    } else if (mag <= 3.0) {
+        color2 = "#5E4352";
+    } else if (mag <=5.0) {
+        color2 = "#E43F6F";
+    } else if (mag <= 7.0) {
+        color2 = "#04471C";
+    } else if (mag <= 9.0) {
+        color2 = "#DFBBB1";
+    } else {
+        color2 = '#058C42';
+    }
+
+    return (color2);
+}
+
+    
